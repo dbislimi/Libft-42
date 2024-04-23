617f7293d3f0b3649fb2744a48aa409f40e45552
Dren Bislimi <dbislimi@student.42nice.fr>
2024-04-23 17:09:56 +0200

    second

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: dbislimi <dbislimi@student.42nice.fr>      #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024-04-22 11:04:58 by dbislimi          #+#    #+#             */
-/*   Updated: 2024-04-22 11:04:58 by dbislimi         ###   ########.fr       */
+/*   Updated: 2024/04/23 15:26:38 by dbislimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,15 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*tab;
 
+	if ((int)nmemb < 0 && (int)size < 0)
+		return (NULL);
 	tab = (void *)malloc(nmemb * size);
 	if (!tab)
 		return (NULL);
 	ft_bzero(tab, nmemb * size);
 	return (tab);
 }
+/*int main(void)
+{
+	printf("%p\n", ft_calloc(-5, -5));
+}*/
